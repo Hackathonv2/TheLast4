9710a4b02bf79d85c77f39c638afc2174171c8c1
Quentin <quentin.charpentier@epitech.eu>
2023-12-21 10:30:15 +0100

    [ADD] Ex1

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -6,3 +6,12 @@ import sys
 lines = []
 for line in sys.stdin:
 	lines.append(line.rstrip('\n'))
+dict = {}
+
+for line in lines[1:]:
+	for word in line.split(' '):
+		if word in dict:
+			dict[word] += 1
+		else:
+			dict[word] = 1
+print(len(dict))
